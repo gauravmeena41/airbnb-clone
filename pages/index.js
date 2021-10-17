3a2e7bb73a1aa71fc3733aa462bc2a236cec4a09
gaurav <gurumeena41.gm@gmail.com>
2021-10-17 19:25:57 +0530

    added small-card, medium-card,large-card components and updated nex.config.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,28 @@
 import Head from "next/head";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import SmallCard from "../components/SmallCard";
+import MediumCard from "../components/MediumCard";
+import LargeCard from "../components/LargeCard";
 
-export const getStaticProps = async () => {};
+export const getStaticProps = async () => {
+  const exploreData = await fetch("https://jsonkeeper.com/b/4G1G").then((res) =>
+    res.json()
+  );
 
-export default function Home() {
+  const cardsData = await fetch("https://jsonkeeper.com/b/VHHT").then((res) =>
+    res.json()
+  );
+
+  return {
+    props: {
+      exploreData,
+      cardsData,
+    },
+  };
+};
+
+export default function Home({ exploreData, cardsData }) {
   return (
     <div>
       <Head>
@@ -18,7 +36,31 @@ export default function Home() {
       <main className="max-w-7xl mx-auto px-8 sm:px-16">
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5 ">Explore Nearby</h2>
+          <div className="grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map(({ img, distance, location }) => (
+              <SmallCard
+                key={img}
+                img={img}
+                distance={distance}
+                location={location}
+              />
+            ))}
+          </div>
         </section>
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Leave anywhere</h2>
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 ml-3 ">
+            {cardsData.map(({ img, title }) => (
+              <MediumCard key={img} img={img} title={title} />
+            ))}
+          </div>
+        </section>
+        <LargeCard
+          img="https://a0.muscache.com/im/pictures/2da67c1c-0c61-4629-8798-1d4de1ac9291.jpg?im_w=1440"
+          title="The Greatest Outdoors"
+          description="Wishlists curated by Airbnb."
+          buttonText="Get inspired"
+        />
       </main>
     </div>
   );
