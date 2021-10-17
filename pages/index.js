65351ca4721259aeb3c58fc75bcfad5f60059657
gaurav <gurumeena41.gm@gmail.com>
2021-10-17 16:28:28 +0530

    added header and banner component in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,25 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+
+export const getStaticProps = async () => {};
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div>
       <Head>
         <title>Airbnb</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Airbnb</h1>
+
+      <Header />
+      <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5 ">Explore Nearby</h2>
+        </section>
+      </main>
     </div>
   );
 }
