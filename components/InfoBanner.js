64bd7d0409dd5986a4e1387bcadf787ff97b8cd9
gaurav <gurumeena41.gm@gmail.com>
2021-10-19 13:44:36 +0530

    added loading bar to the top of the page

--- components/InfoBanner.js
+++ components/InfoBanner.js
@@ -12,7 +12,7 @@ const InfoBanner = ({
   total,
 }) => {
   return (
-    <div className="flex py-7 px-2 pr-4 border-b cursor-pointer hover:opacity-80 hover:shadow-lg transition duration-200 ease-out first:border-t ">
+    <div className="info-card-main">
       <div className="relative h-24 w-40 md:h-52 md:w-80 flex-shrink-0 ">
         <Image
           src={img}
